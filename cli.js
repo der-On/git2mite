f20be3c497c2ace984afe9fbb8ef55ad96bf82c9
Ondrej Brinkel <info@anzui.de>
2015-01-27 23:19:43 +0100

    fixed cli.js

--- cli.js
+++ cli.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
+var path = require('path');
 var jake = require('jake');
 
 var args = process.argv.slice(2);
+args.unshift(path.join(__dirname, 'Jakefile'));
+args.unshift('--jakefile');
 
 jake.run.apply(jake, args);
